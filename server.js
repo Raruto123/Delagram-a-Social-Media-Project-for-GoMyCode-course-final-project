c55c0d3303f8f3290e59a85ef4656063747634a7
sanem9206@gmail.com <mohamedsane@192.168.1.5>
2023-07-21 19:33:21 +0000

    succès avec render

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());//pour lire les req.body
 app.use(bodyParser.urlencoded({extended : true}));//req.params
 app.use(cookieParser()); //req.cookies
 const corsOptions = {
-    origin : ["http://localhost:3001", "https://delagram-app-the-real.onrender.com"],
+    origin : ["http://localhost:3001", "https://delagram-app-ow51.onrender.com"],
     credentials : true,
     "allowedHeaders" : ["sessionId", "Content-Type"],
     "exposedHeaders" : ["sessionId"],
